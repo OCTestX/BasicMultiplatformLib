265e78162749df66bcd4ffad063e919abf026e66
OCTest <octest8848@qq.com>
2025-08-13 09:57:21 -0400

    fix logger2

---
+++ library/src/androidMain/kotlin/logger/OLogger.android.kt
@@ -0,0 +1,19 @@
+package io.github.octestx.basic.multiplatform.common.logger
+
+import android.util.Log
+import io.klogging.Level.*
+import io.klogging.events.LogEvent
+
+actual fun platformPrintLog(log: LogEvent) {
+    val tag = log.context + log.host
+    val message = log.message
+    when (log.level) {
+        TRACE -> Log.v(tag, message)
+        DEBUG -> Log.d(tag, message)
+        INFO -> Log.i(tag, message)
+        WARN -> Log.w(tag, message)
+        ERROR -> Log.e(tag, message)
+        FATAL -> Log.wtf(tag, message)
+        NONE -> {}
+    }
+}
---
+++ library/src/jvmMain/kotlin/logger/OLogger.jvm.kt
@@ -0,0 +1,6 @@
+package io.github.octestx.basic.multiplatform.common.logger
+
+import io.klogging.events.LogEvent
+
+actual fun platformPrintLog(log: LogEvent) {
+}
